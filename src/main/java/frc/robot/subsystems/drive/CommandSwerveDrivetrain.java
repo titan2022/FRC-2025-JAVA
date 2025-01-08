d9ff0e3dd96d08f89ba9ff711a5467f4f7c45346
ethanc8 <echaroenpitaks@gmail.com>
2025-01-08 14:56:33 -0600

    More build fixes for swerve

--- src/main/java/frc/robot/subsystems/drive/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/drive/CommandSwerveDrivetrain.java
@@ -1,4 +1,4 @@
-package frc.robot.subsystems;
+package frc.robot.subsystems.drive;
 
 import static edu.wpi.first.units.Units.*;
 
--- src/main/java/frc/robot/subsystems/drive/TunerConstants.java
+++ src/main/java/frc/robot/subsystems/drive/TunerConstants.java
@@ -1,4 +1,4 @@
-package frc.robot.generated;
+package frc.robot.subsystems.drive;
 
 import static edu.wpi.first.units.Units.*;
 
