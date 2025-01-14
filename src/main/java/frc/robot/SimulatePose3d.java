b9053cc2a20b03ec1b5c2c6817d295482e145a3f
Atharv K <atharv.kanchi@gmail.com>
2025-01-14 15:51:13 -0600

    Updated SimulatePose3D to debug, remove deprecated functions, and fix Pose3d creation

--- src/main/java/frc/robot/SimulatePose3d.java
+++ src/main/java/frc/robot/SimulatePose3d.java
@@ -1,48 +1,50 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.geometry.Rotation3d;
-import org.littletonrobotics.junction.Logger; //team who made it
+import org.littletonrobotics.junction.Logger;
 
-public class SimulatePose3d extends CommandBase {
+public class SimulatePose3d extends Command {
     private Pose3d currentPose;
     private Translation3d direction;
     private double speed; 
     private double period; 
-    private Logger logger = Logger.getInstance();
 
     public SimulatePose3d(Pose3d initialPose, Translation3d direction, double speed, double period) {
         this.currentPose = initialPose;
-        this.direction = direction;
         this.speed = speed;
         this.period = period;
 
         double magnitude = Math.sqrt(direction.getX() * direction.getX() + direction.getY() * direction.getY() + direction.getZ() * direction.getZ());
-        this.direction = new Translation3d(direction.getX() / magnitude, direction.getY() / magnitude, direction.getZ() / magnitude); 
-        
+        if (magnitude != 0) {
+            this.direction = new Translation3d(direction.getX() / magnitude, direction.getY() / magnitude, direction.getZ() / magnitude);
+        } else {
+            this.direction = new Translation3d(0, 0, 0);
+        }
+
         initialize();
-        for(int i = 0; i < 100; i++){
+        for (int i = 0; i < 100; i++) {
             execute();
         }        
     }
     
     @Override
     public void initialize() {
-        logger.recordOutput("PoseSimulation/InitialPose", currentPose);
+        Logger.recordOutput("PoseSimulation/InitialPose", currentPose);
     }
 
     @Override
     public void execute() {
-        Translation3d displacement = direction.add(speed * period);
+        Translation3d displacement = direction.times(speed * period);
 
         currentPose = new Pose3d(
                 currentPose.getTranslation().plus(displacement),
                 currentPose.getRotation()
         );
 
-        logger.recordOutput("PoseSimulation/CurrentPose", currentPose);
+        Logger.recordOutput("PoseSimulation/CurrentPose", currentPose);
     }
 
     @Override
@@ -53,11 +55,15 @@ public class SimulatePose3d extends CommandBase {
     @Override
     public void end(boolean interrupted) {
         if (interrupted) {
-            logger.recordOutput("PoseSimulation/Status", "Interrupted");
+            Logger.recordOutput("PoseSimulation/Status", "Interrupted");
         } else {
-            logger.recordOutput("PoseSimulation/Status", "Completed");
+            Logger.recordOutput("PoseSimulation/Status", "Completed");
         }
     }
 
-    SimulatePose3D tester = new SimulatePose3D(Pose3d(0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5),Translation3d(1.5, 1.5, 1.5), 2, 2);
+    SimulatePose3d tester = new SimulatePose3d(
+        new Pose3d(new Translation3d(0.5, 0.5, 0.5), new Rotation3d(0.5, 0.5, 0.5)),
+        new Translation3d(1.5, 1.5, 1.5), 
+        2, 2
+    );
 }
