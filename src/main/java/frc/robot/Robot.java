8bb5dcbe60fc295322a18b42ef5d83804b8a1680
nekiwo <58634064+nekiwo@users.noreply.github.com>
2025-01-17 12:05:54 -0600

    cleanup

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,154 +4,87 @@
 
 package frc.robot;
 
-import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
-import com.ctre.phoenix6.swerve.SwerveModule.SteerRequestType;
-import com.ctre.phoenix6.swerve.SwerveRequest;
-
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.subsystems.drive.RotationalDrivebase;
-import frc.robot.subsystems.drive.TranslationalDrivebase;
 import frc.robot.subsystems.drive.YAGSLSwerveDrivetrain;
 import frc.robot.utility.Constants.Unit;
-import swervelib.SwerveInputStream;
 import swervelib.math.SwerveMath;
 
 public class Robot extends TimedRobot {
-  public static class OperatorConstants
-  {
+	public final CommandXboxController driveController = new CommandXboxController(0);
 
-    // Joystick Deadband
-    public static final double DEADBAND        = 0.1;
-    public static final double LEFT_Y_DEADBAND = 0.1;
-    public static final double RIGHT_X_DEADBAND = 0.1;
-    public static final double TURN_CONSTANT    = 6;
-  }
+	public final YAGSLSwerveDrivetrain drivetrain = new YAGSLSwerveDrivetrain();
 
-  // private RobotContainer robotContainer;
-  
-  public final CommandXboxController driveController = new CommandXboxController(0); // My joystick
-  public final CommandXboxController robotController = new CommandXboxController(1); // My joystick
+	@Override
+	public void robotInit() {
+		SmartDashboard.putNumber("angleFactor", SwerveMath.calculateDegreesPerSteeringRotation(21.428571428571427));
+		SmartDashboard.putNumber("driveFactor", SwerveMath.calculateMetersPerRotation(4 * Unit.IN, 6.122448979591837));
+	}
 
-  public final YAGSLSwerveDrivetrain drivetrain = new YAGSLSwerveDrivetrain(); // My drivetrain
-  // public final TranslationalDrivebase translationalDrivetrain = drivetrain.getTranslational();
-  // public final RotationalDrivebase rotationalDrivebase = drivetrain.getRotational();
+	@Override
+	public void robotPeriodic() {
+		CommandScheduler.getInstance().run();
+	}
 
-   SwerveInputStream driveAngularVelocity = SwerveInputStream.of(drivetrain.getSwerveDrive(),
-                                                                () -> driveController.getLeftY() * -1,
-                                                                () -> driveController.getLeftX() * -1)
-                                                            .withControllerRotationAxis(driveController::getRightX)
-                                                            .deadband(OperatorConstants.DEADBAND)
-                                                            .scaleTranslation(0.8);
-                                                            // .allianceRelativeControl(true);
+	@Override
+	public void disabledInit() {
+	}
 
-  SwerveInputStream driveDirectAngle = driveAngularVelocity.copy().withControllerHeadingAxis(driveController::getRightX,
-  driveController::getRightY).headingWhile(false);
- 
-  private static final double XBOX_DEADBAND = 0.2;
-  private double deadband(double input) {
-      if (Math.abs(input) > XBOX_DEADBAND)
-          return input;
-      else 
-          return 0;
-  }
+	@Override
+	public void disabledPeriodic() {
+	}
 
-  @Override
-  public void robotInit() {
-    // robotContainer = new RobotContainer();
+	@Override
+	public void disabledExit() {
+	}
 
-    SmartDashboard.putNumber("angleFactor", SwerveMath.calculateDegreesPerSteeringRotation(21.428571428571427));
-    SmartDashboard.putNumber("driveFactor", SwerveMath.calculateMetersPerRotation(4 * Unit.IN, 6.122448979591837));
-  }
+	@Override
+	public void autonomousInit() {
 
-  @Override
-  public void robotPeriodic() {
-    CommandScheduler.getInstance().run(); 
+	}
 
-    // SmartDashboard.putNumber("curr_velx", translationalDrivetrain.getVelocity().getX());
-    // SmartDashboard.putNumber("curr_vely", translationalDrivetrain.getVelocity().getY());
-    // SmartDashboard.putNumber("curr_omega", rotationalDrivebase.getRotationalVelocity().getDegrees());
-  }
+	@Override
+	public void autonomousPeriodic() {
+		drivetrain.drive(new Translation2d(1, 0), 0, false);
+	}
 
-  @Override
-  public void disabledInit() {}
+	@Override
+	public void autonomousExit() {
+	}
 
-  @Override
-  public void disabledPeriodic() {}
+	@Override
+	public void teleopInit() {
+		// translationalDrivetrain.setDefaultCommand(translationalDrivetrain.translationalDrive(driveController));
+		// rotationalDrivebase.setDefaultCommand(rotationalDrivebase.rotationalDrive(driveController));
+	}
 
-  @Override
-  public void disabledExit() {}
+	@Override
+	public void teleopPeriodic() {
+		drivetrain.drive(new Translation2d(0, 1), 0, false);
+	}
 
-  @Override
-  public void autonomousInit() {
-    // translationalDrivetrain.removeDefaultCommand();
-    // rotationalDrivebase.removeDefaultCommand();
-    // m_autonomousCommand = robotContainer.getAutonomousCommand();
+	@Override
+	public void teleopExit() {
 
-    // if (m_autonomousCommand != null) {
-    //   m_autonomousCommand.schedule();
-    // }
-    
-  }
+	}
 
-  @Override
-  public void autonomousPeriodic() {
-    // translationalDrivetrain.setVelocity(new Translation2d(1, 0));
-    // rotationalDrivebase.setRotationalVelocity(new Rotation2d(1));
+	@Override
+	public void testInit() {
+		CommandScheduler.getInstance().cancelAll();
+	}
 
-    drivetrain.drive(new Translation2d(1, 0), 0, false);
-  }
+	@Override
+	public void testPeriodic() {
+	}
 
-  @Override
-  public void autonomousExit() {}
+	@Override
+	public void testExit() {
+	}
 
-  @Override
-  public void teleopInit() {
-    //translationalDrivetrain.setDefaultCommand(translationalDrivetrain.translationalDrive(driveController));
-    //rotationalDrivebase.setDefaultCommand(rotationalDrivebase.rotationalDrive(driveController));
-
-    // Command driveFieldOrientedDirectAngle         = drivetrain. (driveDirectAngle);
-    // Command driveFieldOrientedAnglularVelocity    = drivetrain.driveFieldOriented(driveAngularVelocity);
-
-    // drivetrain.setDefaultCommand(driveFieldOrientedAnglularVelocity);
-  }
-
-  @Override
-  public void teleopPeriodic() {
-    // translationalDrivetrain.setVelocity(new Translation2d(deadband(driveController.getLeftX()) * 0.5, deadband(driveController.getLeftY()) * 0.5));
-    // rotationalDrivebase.setRotationalVelocity(new Rotation2d(deadband(driveController.getRightX())));
-
-    // drivetrain.setControl(
-    //   m_driveRequest.withVelocityX(-driveController.getLeftY())
-    //      .withVelocityY(-driveController.getLeftX())
-    //      .withRotationalRate(-driveController.getRightX())
-    // );
-
-    
-    drivetrain.drive(new Translation2d(0, 1), 0, false);
-  }
-
-  @Override
-  public void teleopExit() {
-
-  }
-
-  @Override
-  public void testInit() {
-    CommandScheduler.getInstance().cancelAll();
-  }
-
-  @Override
-  public void testPeriodic() {}
-
-  @Override
-  public void testExit() {}
-
-  @Override
-  public void simulationPeriodic() {}
+	@Override
+	public void simulationPeriodic() {
+	}
 }
--- src/main/java/frc/robot/subsystems/drive/YAGSLSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/drive/YAGSLSwerveDrivetrain.java
@@ -7,14 +7,11 @@ import java.util.function.DoubleSupplier;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.utility.Constants;
 import swervelib.parser.SwerveDriveConfiguration;
 import swervelib.parser.SwerveParser;
 import swervelib.SwerveController;
 import swervelib.SwerveDrive;
 import swervelib.math.SwerveMath;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
@@ -25,7 +22,7 @@ public class YAGSLSwerveDrivetrain extends SubsystemBase {
 
     public YAGSLSwerveDrivetrain() {
         try {
-            swerveDrive = new SwerveParser(swerveJsonDirectory).createSwerveDrive(TunerConstants.MAX_SPEED);
+            swerveDrive = new SwerveParser(swerveJsonDirectory).createSwerveDrive(5.0);
         } catch (Exception e) {
             System.err.println(
                     "Could not create swerve drive. Make sure src/main/deploy/swerve/controllerproperties.json, src/main/deploy/swerve/modules, and src/main/deploy/swerve/swervedrive.json exist.");
@@ -36,91 +33,6 @@ public class YAGSLSwerveDrivetrain extends SubsystemBase {
         swerveDrive.setModuleEncoderAutoSynchronize(false, 1);
         // swerveDrive.pushOffsetsToEncoders();
     }
-
-    private static final double XBOX_DEADBAND = 0.2;
-
-    private double deadband(double input) {
-        if (Math.abs(input) > XBOX_DEADBAND)
-            return input;
-        else
-            return 0;
-    }
-
-    // private final TranslationalDrivebase translational = new
-    // TranslationalDrivebase() {
-    // @Override
-    // public void setVelocity(Translation2d velocity) {
-    // ChassisSpeeds currentSpeeds = swerveDrive.getRobotVelocity();
-    // setVelocities(new ChassisSpeeds(velocity.getX(), velocity.getY(),
-    // currentSpeeds.omegaRadiansPerSecond));
-    // }
-
-    // @Override
-    // public Translation2d getVelocity() {
-    // ChassisSpeeds speeds = swerveDrive.getRobotVelocity();
-    // return new Translation2d(speeds.vxMetersPerSecond, speeds.vyMetersPerSecond);
-    // }
-
-    // @Override
-    // public Command translationalDrive(CommandXboxController xbox) {
-    // return run(() -> {
-    // double v_x = deadband(xbox.getLeftX());
-    // double v_y = deadband(xbox.getLeftY());
-    // // double magnitude = Math.sqrt(v_x * v_x + v_y * v_y);
-    // // if (magnitude > TunerConstants.MAX_SPEED) {
-    // // v_x *= TunerConstants.MAX_SPEED / magnitude;
-    // // v_y *= TunerConstants.MAX_SPEED / magnitude;
-    // // }
-    // v_x *= TunerConstants.MAX_SPEED;
-    // v_y *= TunerConstants.MAX_SPEED;
-    // setVelocity(new Translation2d(v_x, v_y));
-    // });
-    // }
-
-    // // @Override
-    // // public Command translationalDrive(CommandXboxController xbox) {
-    // // return new TranslationalDriveCommand(translational, xbox.getHID(),
-    // // TunerConstants.MAX_SPEED);
-    // // }
-    // };
-
-    // public TranslationalDrivebase getTranslational() {
-    // return translational;
-    // }
-
-    // private final RotationalDrivebase rotational = new RotationalDrivebase() {
-    // @Override
-    // public void setRotationalVelocity(Rotation2d omega) {
-    // ChassisSpeeds currentSpeeds = swerveDrive.getRobotVelocity();
-    // setVelocities(new ChassisSpeeds(currentSpeeds.vxMetersPerSecond,
-    // currentSpeeds.vxMetersPerSecond,
-    // omega.getRadians()));
-    // }
-
-    // @Override
-    // public Rotation2d getRotationalVelocity() {
-    // ChassisSpeeds speeds = swerveDrive.getRobotVelocity();
-    // return new Rotation2d(speeds.omegaRadiansPerSecond);
-    // }
-
-    // @Override
-    // public Command rotationalDrive(CommandXboxController xbox) {
-    // return run(() -> {
-    // setRotationalVelocity(new Rotation2d(TunerConstants.MAX_ANGULAR_SPEED *
-    // deadband(xbox.getRightX())));
-    // });
-    // }
-
-    // // @Override
-    // // public Command rotationalDrive(CommandXboxController xbox) {
-    // // return new RotationalDriveCommand(rotational, xbox.getHID(),
-    // // TunerConstants.MAX_ANGULAR_SPEED);
-    // // }
-    // };
-
-    // public RotationalDrivebase getRotational() {
-    // return rotational;
-    // }
 
     public void setVelocities(ChassisSpeeds speeds) {
         swerveDrive.drive(speeds);
@@ -135,14 +47,11 @@ public class YAGSLSwerveDrivetrain extends SubsystemBase {
     }
 
     public Command centerModulesCommand() {
-        return run(() -> Arrays.asList(swerveDrive.getModules())
-                .forEach(it -> it.setAngle(0.0)));
+        return run(() -> Arrays.asList(swerveDrive.getModules()).forEach(it -> it.setAngle(0.0)));
     }
 
-    public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY,
-            DoubleSupplier angularRotationX) {
+    public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY, DoubleSupplier angularRotationX) {
         return run(() -> {
-            // Make the robot move
             swerveDrive.drive(SwerveMath.scaleTranslation(new Translation2d(
                     translationX.getAsDouble() * swerveDrive.getMaximumChassisVelocity(),
                     translationY.getAsDouble() * swerveDrive.getMaximumChassisVelocity()), 0.8),
@@ -153,10 +62,7 @@ public class YAGSLSwerveDrivetrain extends SubsystemBase {
     }
 
     public void drive(Translation2d translation, double rotation, boolean fieldRelative) {
-        swerveDrive.drive(translation,
-                rotation,
-                fieldRelative,
-                false); // Open loop is disabled since it shouldn't be used most of the time.
+        swerveDrive.drive(translation, rotation, fieldRelative, false);
     }
 
     public void drive(ChassisSpeeds velocity) {
@@ -170,19 +76,6 @@ public class YAGSLSwerveDrivetrain extends SubsystemBase {
     public void setChassisSpeeds(ChassisSpeeds chassisSpeeds) {
         swerveDrive.setChassisSpeeds(chassisSpeeds);
     }
-
-    // public ChassisSpeeds getTargetSpeeds(double xInput, double yInput, double
-    // headingX, double headingY)
-    // {
-    // Translation2d scaledInputs = SwerveMath.cubeTranslation(new
-    // Translation2d(xInput, yInput));
-    // return swerveDrive.swerveController.getTargetSpeeds(scaledInputs.getX(),
-    // scaledInputs.getY(),
-    // headingX,
-    // headingY,
-    // getHeading().getRadians(),
-    // Constants.MAX_SPEED);
-    // }
 
     public ChassisSpeeds getRobotVelocity() {
         return swerveDrive.getRobotVelocity();
