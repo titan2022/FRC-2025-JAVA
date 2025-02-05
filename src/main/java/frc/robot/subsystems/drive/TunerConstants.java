a26c518f2a878fd472a36d7bf9b8c7b17f9053ff
SofiaOrj <2010.sofia.orjuela@gmail.com>
2025-02-04 18:52:46 -0600

    Increase max speed to 10 rad/s

--- src/main/java/frc/robot/subsystems/drive/TunerConstants.java
+++ src/main/java/frc/robot/subsystems/drive/TunerConstants.java
@@ -23,7 +23,7 @@ public class TunerConstants {
     /// The max speed, in meters per second
     public static final double MAX_SPEED = 1; // m/s
     /// The max angular speed, in radians per second
-    public static final double MAX_ANGULAR_SPEED = 5 * Unit.DEG; // rad/s
+    public static final double MAX_ANGULAR_SPEED = 10 * Unit.DEG; // rad/s
 
     ///////////////////////////////////////
     ///// Generated stuff begins here /////
