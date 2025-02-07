c88e38c4d3665ac3ed812ea03bf4e255414d14b6
ethanc8 <echaroenpitaks@gmail.com>
2025-02-06 19:14:51 -0600

    Import AdvantageKit-style vision subsystem

---
+++ src/main/java/frc/robot/subsystems/vision/VisionIO.java
@@ -0,0 +1,41 @@
+// Based on https://github.com/Mechanical-Advantage/AdvantageKit/releases/download/v4.1.0/AdvantageKit_VisionTemplate.zip
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: 2025 FRC#2022
+//                         2021-2025 FRC#6328
+
+package frc.robot.subsystems.vision;
+
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+// import org.littletonrobotics.junction.AutoLog;
+
+public interface VisionIO {
+  // @AutoLog
+  public static class VisionIOInputs {
+    public boolean connected = false;
+    public TargetObservation latestTargetObservation =
+        new TargetObservation(new Rotation2d(), new Rotation2d());
+    public PoseObservation[] poseObservations = new PoseObservation[0];
+    public int[] tagIds = new int[0];
+  }
+
+  /** Represents the angle to a simple target, not used for pose estimation. */
+  public static record TargetObservation(Rotation2d tx, Rotation2d ty) {}
+
+  /** Represents a robot pose sample used for pose estimation. */
+  public static record PoseObservation(
+      double timestamp,
+      Pose3d pose,
+      double ambiguity,
+      int tagCount,
+      double averageTagDistance,
+      PoseObservationType type) {}
+
+  public static enum PoseObservationType {
+    MEGATAG_1,
+    MEGATAG_2,
+    PHOTONVISION
+  }
+
+  public default void updateInputs(VisionIOInputs inputs) {}
+}
