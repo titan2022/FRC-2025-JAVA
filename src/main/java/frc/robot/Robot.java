a728623eaeb0d27519429f2b0f98b86072acdc78
Ethan C <echaroenpitaks@gmail.com>
2025-02-10 16:56:03 -0600

    Add kitbot code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,90 +1,38 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import edu.wpi.first.math.geometry.Translation2d;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.subsystems.drive.YAGSLSwerveDrivetrain;
-import frc.robot.utility.Constants.Unit;
-import swervelib.math.SwerveMath;
 
 public class Robot extends TimedRobot {
-	public final CommandXboxController driveController = new CommandXboxController(0);
+    private double kP = 0.5;
 
-	public final YAGSLSwerveDrivetrain drivetrain = new YAGSLSwerveDrivetrain();
+    private WPI_TalonSRX m1 = new WPI_TalonSRX(7); // Front left
+    private WPI_TalonSRX m2 = new WPI_TalonSRX(1); // Back left
+    private WPI_TalonSRX m3 = new WPI_TalonSRX(50); // Front right
+    private WPI_TalonSRX m4 = new WPI_TalonSRX(8); // Back Right
 
-	@Override
-	public void robotInit() {
-		SmartDashboard.putNumber("angleFactor", SwerveMath.calculateDegreesPerSteeringRotation(21.428571428571427));
-		SmartDashboard.putNumber("driveFactor", SwerveMath.calculateMetersPerRotation(4 * Unit.IN, 6.122448979591837));
-	}
+    private final XboxController xbox = new XboxController(0);
 
-	@Override
-	public void robotPeriodic() {
-		CommandScheduler.getInstance().run();
-	}
+    @Override
+    public void robotInit() {
+        m1.setInverted(true);
+        m2.setInverted(true);
+    }
 
-	@Override
-	public void disabledInit() {
-	}
+    @Override
+    public void teleopPeriodic() {
+        SmartDashboard.putNumber("right x", xbox.getRightY());
+        SmartDashboard.putNumber("left y", xbox.getLeftY());
+        SmartDashboard.putBoolean("A", xbox.getAButtonPressed());
+        SmartDashboard.putBoolean("B", xbox.getBButtonPressed());
 
-	@Override
-	public void disabledPeriodic() {
-	}
-
-	@Override
-	public void disabledExit() {
-	}
-
-	@Override
-	public void autonomousInit() {
-
-	}
-
-	@Override
-	public void autonomousPeriodic() {
-		drivetrain.drive(new Translation2d(1, 0), 0, false);
-	}
-
-	@Override
-	public void autonomousExit() {
-	}
-
-	@Override
-	public void teleopInit() {
-		// translationalDrivetrain.setDefaultCommand(translationalDrivetrain.translationalDrive(driveController));
-		// rotationalDrivebase.setDefaultCommand(rotationalDrivebase.rotationalDrive(driveController));
-	}
-
-	@Override
-	public void teleopPeriodic() {
-		drivetrain.drive(new Translation2d(0, 1), 0, false);
-	}
-
-	@Override
-	public void teleopExit() {
-
-	}
-
-	@Override
-	public void testInit() {
-		CommandScheduler.getInstance().cancelAll();
-	}
-
-	@Override
-	public void testPeriodic() {
-	}
-
-	@Override
-	public void testExit() {
-	}
-
-	@Override
-	public void simulationPeriodic() {
-	}
+        m1.set(ControlMode.PercentOutput, kP * xbox.getLeftY());
+        m2.set(ControlMode.PercentOutput, kP * xbox.getLeftY());
+        m3.set(ControlMode.PercentOutput, kP * xbox.getRightY());
+        m4.set(ControlMode.PercentOutput, kP * xbox.getRightY());
+    }
 }
