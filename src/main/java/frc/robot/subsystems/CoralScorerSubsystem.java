3625ce2242575262e318967e03cd0b877ed3f740
ethanc8 <echaroenpitaks@gmail.com>
2025-02-12 15:59:08 -0600

    Create coral scorer and implement coral scoring

---
+++ src/main/java/frc/robot/subsystems/CoralScorerSubsystem.java
@@ -0,0 +1,58 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.reduxrobotics.sensors.canandcolor.Canandcolor;
+
+public class CoralScorerSubsystem extends SubsystemBase {
+  private static final TalonFX scoringMotor = new TalonFX(0, "CANivore");
+  private static final TalonFX dealgifierMotor = new TalonFX(0, "CANivore");
+  private static final Canandcolor canandcolor = new Canandcolor(0);
+
+  // TODO: Measure this
+  private static final double PROXIMITY_THRESHOLD = 0.5;
+
+  // TODO: Determine speed
+  private static final double MOVE_CORAL_SPEED = 0.5; // out of 1.0
+  private static final double SCORE_CORAL_SPEED = 1.0; // out of 1.0
+
+  public CoralScorerSubsystem() {
+    // Brake the scoring motor while not in use
+    scoringMotor.setNeutralMode(NeutralModeValue.Brake);
+    // Allow the dealgifier to coast while not in use
+    dealgifierMotor.setNeutralMode(NeutralModeValue.Coast);
+  }
+
+  public boolean canContactCanandcolor() {
+    return canandcolor.isConnected();
+  }
+
+  public boolean canSeeProximity() {
+    return canandcolor.getProximity() > PROXIMITY_THRESHOLD;
+  }
+
+  /** Start moving coral into the scorer
+   */
+  public void moveCoral() {
+    scoringMotor.set(MOVE_CORAL_SPEED);
+  }
+
+  /** Start scoring coral into the scorer
+   */
+  public void scoreCoral() {
+    scoringMotor.set(SCORE_CORAL_SPEED);
+  }
+
+  /** Move coral back into the elevator
+   */
+  public void reverseMoveCoral() {
+    scoringMotor.set(-MOVE_CORAL_SPEED);
+  }
+  
+  /** Stop moving coral
+   */
+  public void stopMovingCoral() {
+    scoringMotor.set(0);
+  }
+}
