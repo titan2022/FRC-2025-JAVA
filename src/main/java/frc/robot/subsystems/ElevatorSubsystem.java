00de82d5bd99c964e64d4edbd06fd3713edbc0b5
ethanc8 <echaroenpitaks@gmail.com>
2025-02-15 21:27:39 -0600

    Use REV through-bore encoder for elevator

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -11,15 +12,17 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 public class ElevatorSubsystem extends SubsystemBase {
+  // We have two Falcon 500s
+  // TODO: Specify CAN IDs
   private static final TalonFX leftMotor = new TalonFX(0, "CANivore");
   private static final TalonFX rightMotor = new TalonFX(0, "CANivore");
+
+  // We have a REV through-bore encoder
+  // TODO: Specify DIO channels
+  // Programming manual: https://docs.wpilib.org/en/stable/docs/software/hardware-apis/sensors/encoders-software.html#quadrature-encoders-the-encoder-class
+  private static final Encoder encoder = new Encoder(0, 0);
+
   private static final PIDController pid = new PIDController(1.0, 0.0, 0.0);
-
-  private double initialLeft = 0.0;
-  private double initialRight = 0.0;
-
-  private StatusSignal<Angle> leftPositionStatusSignal;
-  private StatusSignal<Angle> rightPositionStatusSignal;
 
   // TODO: Figure out good values for these constants
   // The unit is rotations of the motor (rotor, excluding gear ratio)
@@ -46,25 +49,17 @@ public class ElevatorSubsystem extends SubsystemBase {
         .andThen(run(() -> {}))
         .withName("Idle"));
 
-    leftPositionStatusSignal = leftMotor.getRotorPosition();
-    rightPositionStatusSignal = rightMotor.getRotorPosition();
+    
 
     resetMeasurement();
   }
 
   public void resetMeasurement() {
-    leftPositionStatusSignal.refresh();
-    initialLeft = leftPositionStatusSignal.getValueAsDouble();
-    rightPositionStatusSignal.refresh();
-    initialRight = rightPositionStatusSignal.getValueAsDouble();
+    encoder.reset();
   }
 
   public double getMeasurement() {
-    double left = leftPositionStatusSignal.getValueAsDouble();
-    double right = rightPositionStatusSignal.getValueAsDouble();
-    double leftMeasurement = left - initialLeft;
-    double rightMeasurement = right - initialRight;
-    return (leftMeasurement + rightMeasurement)/2;
+    return encoder.getDistance();
   }
 
   public enum ElevationTarget {
