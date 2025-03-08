6a9fcb8d40432de00257a5016c8b3648126b3a50
nekiwo <58634064+nekiwo@users.noreply.github.com>
2025-03-08 17:20:43 -0600

    coral shifting

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.CoralIntakeCommand;
 import frc.robot.commands.drive.DrivingCommand;
@@ -83,6 +84,9 @@ public class Robot extends TimedRobot {
     robotController.b().whileTrue(
       coralScorer.timedScoreCoralCommand(true)
     );
+
+    // Shifting coral forward when elevator passes bumper
+    coralScorer.setDefaultCommand(coralScorer.coralShiftingCommand(elevator));
 
     // Elevator controls
     // Left dpad is elevate to coral intake level
--- src/main/java/frc/robot/subsystems/CoralScorerSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralScorerSubsystem.java
@@ -1,51 +1,61 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Robot;
-import frc.robot.utility.Constants.Unit;
-
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.reduxrobotics.sensors.canandcolor.Canandcolor;
 
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
 public class CoralScorerSubsystem extends SubsystemBase {
-  // TODO: Specify CAN IDs
+  private static final double PROXIMITY_THRESHOLD = 0.020;
+  private static final double MOVE_CORAL_SPEED = 1.8; // in volts
+  private static final double SCORE_CORAL_SPEED = 1.5; // in volts
+  private static final long SCORE_CORAL_TIMEOUT = 1 * 1000000; // microseconds
+  private static final long INDEX_CORAL_TIMEOUT = 1 * 150000; // microseconds
+  // TODO: determine this
+  private static final double CORAL_SHIFT_ELEVATOR_HEIGHT_INCHES = 15.0;
+  
   private static final TalonFX scoringMotor = new TalonFX(42, "rio");
   private static final Canandcolor canandcolor = new Canandcolor(0);
 
-  private static final double PROXIMITY_THRESHOLD = 0.020;
-
-  // TODO: Determine speed
-  private static final double MOVE_CORAL_SPEED = 1.8; // in volts
-  private static final double SCORE_CORAL_SPEED = 1.5; // in volts
-
-  private static final long SCORE_CORAL_TIMEOUT = 1 * 1000000; // microseconds
-  private static final long INDEX_CORAL_TIMEOUT = 1 * 150000; // microseconds
-
   private long coralVisibleStart;
+  private double elevatorHeight = 0;
   private boolean sawCoralInLastFrame;
+  private boolean coralShifted = false; // For shifting coral forward after elevator is above the bumper
+  private boolean coralMoving = false; // Ignore shifting if it's being moved
+  private boolean coralMovedAfterShift = false; // Don't shift back if user already shifted the coral
 
   public CoralScorerSubsystem() {
+
     // Brake the scoring motor while not in use
     scoringMotor.setNeutralMode(NeutralModeValue.Brake);
 
-    setDefaultCommand(
-      runOnce(
-        () -> {
-          stopMovingCoral();
-        }
-      )
-      .andThen(run(() -> {}))
-      .withName("Idle")
-    );
+    // Why was this here?
+    // setDefaultCommand(
+    //   runOnce(
+    //     () -> {
+    //       stopMovingCoral();
+    //     }
+    //   )
+    //   .andThen(run(() -> {}))
+    //   .withName("Idle")
+    // );
   }
 
   @Override
   public void periodic() {
     SmartDashboard.putNumber("Canandcolor proximity", canandcolor.getProximity());
+  }
+
+  public void resetCoralShifting() {
+    coralMovedAfterShift = false;
+    coralShifted = false;
   }
 
   public boolean canContactCanandcolor() {
@@ -79,12 +89,20 @@ public class CoralScorerSubsystem extends SubsystemBase {
   /** Start moving coral into the scorer
    */
   public void moveCoral() {
+    coralMoving = true;
+    if (coralShifted) {
+      coralMovedAfterShift = true;
+    }
     scoringMotor.setVoltage(MOVE_CORAL_SPEED);
   }
 
   /** Start scoring coral into the scorer
    */
   public void scoreCoral(boolean isReversed) {
+    coralMoving = true;
+    if (coralShifted) {
+      coralMovedAfterShift = true;
+    }
     scoringMotor.setVoltage(SCORE_CORAL_SPEED * (isReversed ? -0.75 : 1));
   }
 
@@ -107,8 +125,60 @@ public class CoralScorerSubsystem extends SubsystemBase {
     }
 
     @Override
+    public void end(boolean isInterrupted) {
+      stopMovingCoral();
+    }
+
+    @Override
     public boolean isFinished() {
       return false;//RobotController.getFPGATime() >= startTime + SCORE_CORAL_TIMEOUT;
+    }
+  }
+
+  private class CoralShiftingCommand extends Command {
+    private CoralScorerSubsystem coralScorer;
+    private ElevatorSubsystem elevator;
+
+    public CoralShiftingCommand(CoralScorerSubsystem coralScorer, ElevatorSubsystem elevator) {
+      this.coralScorer = coralScorer;
+      this.elevator = elevator;
+
+      addRequirements(coralScorer);
+    }
+
+    @Override
+    public void initialize() {
+      new Trigger(() -> !coralMoving && !coralMovedAfterShift && elevatorHeight >= CORAL_SHIFT_ELEVATOR_HEIGHT_INCHES && !coralShifted).onTrue(
+        new SequentialCommandGroup(
+          Commands.runOnce(() -> {coralShifted = true;}),
+          timedScoreCoralCommand(false).withTimeout(0.0)
+        )
+      );
+      
+      new Trigger(() -> !coralMoving && !coralMovedAfterShift && elevatorHeight < CORAL_SHIFT_ELEVATOR_HEIGHT_INCHES && coralShifted).onTrue(
+        new SequentialCommandGroup(
+          Commands.runOnce(() -> {coralShifted = false;}),
+          timedScoreCoralCommand(true).withTimeout(0.0)
+        )
+      );
+    }
+
+    @Override
+    public void execute() {
+      elevatorHeight = elevator.getElevatorPosition();
+      if (elevatorHeight < CORAL_SHIFT_ELEVATOR_HEIGHT_INCHES) {
+        resetCoralShifting();
+      }
+    }
+
+    @Override
+    public void end(boolean isInterrupted) {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+      return false;
     }
   }
 
@@ -118,15 +188,27 @@ public class CoralScorerSubsystem extends SubsystemBase {
     return new TimedScoreCoralCommand(this, isReversed);
   }
 
+  /*
+   * Command that shifts the coral after the elevator passes the bumper.
+   */
+  public Command coralShiftingCommand(ElevatorSubsystem elevator) {
+    return new CoralShiftingCommand(this, elevator);
+  }
+
   /** Move coral back into the elevator
    */
   public void reverseMoveCoral() {
+    coralMoving = true;
+    if (coralShifted) {
+      coralMovedAfterShift = true;
+    }
     scoringMotor.setVoltage(-MOVE_CORAL_SPEED);
   }
   
   /** Stop moving coral
    */
   public void stopMovingCoral() {
+    coralMoving = false;
     scoringMotor.stopMotor();
   }
 }
