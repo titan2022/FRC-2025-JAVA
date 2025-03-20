b52460623209e3c78da03f2948c20509d1305bf0
titan2022-multiuser-system <153474769+titan2022-multiuser-system@users.noreply.github.com>
2025-03-20 11:49:57 -0500

    Reduce stddevs to 4 * distance

--- src/main/java/frc/robot/utility/OdometryLocalizer.java
+++ src/main/java/frc/robot/utility/OdometryLocalizer.java
@@ -44,8 +44,8 @@ public class OdometryLocalizer extends Localizer {
       return;
     }
     Matrix<N3, N1> stdDevs = new Matrix<N3, N1>(Nat.N3(), Nat.N1());
-    stdDevs.set(0, 0, 15.0 * measurement.distance);
-    stdDevs.set(1, 0, 15.0 * measurement.distance);
+    stdDevs.set(0, 0, 4.0 * measurement.distance);
+    stdDevs.set(1, 0, 4.0 * measurement.distance);
     stdDevs.set(2, 0, 1.0 * Double.POSITIVE_INFINITY);
     drivetrain.addVisionMeasurement(measurement.pose, measurement.measurementTime, stdDevs);
   }
