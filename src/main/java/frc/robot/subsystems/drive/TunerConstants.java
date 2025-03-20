d381437285fd696a008a76ee1df8e340a3439394
titan2022-multiuser-system <153474769+titan2022-multiuser-system@users.noreply.github.com>
2025-03-20 17:26:27 -0500

    test match starting 17:16

--- src/main/java/frc/robot/subsystems/drive/TunerConstants.java
+++ src/main/java/frc/robot/subsystems/drive/TunerConstants.java
@@ -97,7 +97,7 @@ public class TunerConstants {
 
     private static final double kDriveGearRatio = 6.746031746031747;
     private static final double kSteerGearRatio = 21.428571428571427;
-    private static final Distance kWheelRadius = Inches.of(2);
+    private static final Distance kWheelRadius = Inches.of(1.95);
 
     private static final boolean kInvertLeftSide = false;
     private static final boolean kInvertRightSide = true;
